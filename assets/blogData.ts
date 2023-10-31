93ae8b0693c2ea83bf302be8f1710c26589f6034
thiebautremy <remythiebaut@msn.com>
2023-10-31 10:50:20 +0100

    add article 2

--- assets/blogData.ts
+++ assets/blogData.ts
@@ -3,8 +3,15 @@ import code from "@/assets/blog/code.jpg";
 const article1 = {
   title: "Comment créer un site internet à Pont-à-mousson ?",
   catchPhrase:
-    "<p>Avoir un site internet est aujourd'hui indispensable pour se faire connaître et développer son activité.</p>",
+    "<p>Il existe de nombreuses solutions afin de créer un site web aujourd'hui.</p>",
   text: "<p className='textHighlighted'>Avoir un site internet est aujourd'hui indispensable pour toute entreprise ou professionnel qui souhaite se faire connaître et développer son activité. Mais créer un site internet peut s'avérer complexe, surtout si l'on n'est pas du métier.</p><h2>Les différentes solutions :</h2><h3>Faire appel à une agence web :</h3><p>C'est la solution la plus simple et la plus rapide. Une agence web se chargera de toutes les étapes de la création de votre site, de la conception à la mise en ligne en passant par le référencement (SEO) et la sécurité.</p><h3>Créer son site soi-même :</h3><p>C'est la solution la plus économique, mais elle demande un certain niveau de compétences techniques. Il existe de nombreux outils et plateformes qui permettent de créer un site internet sans avoir besoin de connaissances en programmation.</p><h2>Les difficultés techniques :</h2><h3>La conception :</h3><p>Il faut d'abord concevoir le site internet, c'est-à-dire définir son architecture, son design et son contenu. Cette étape peut être complexe, surtout si vous n'avez pas d'expérience en webdesign.</p><h3>Le développement :</h3><p>Une fois la conception terminée, il faut développer le site internet. Cette étape consiste à écrire le code qui permettra au site de fonctionner. Si vous n'avez pas de connaissances en programmation, vous pouvez utiliser un outil de création de site internet en ligne.</p><h3>L'hébergement :</h3><p>Une fois le site internet développé, il faut le mettre en ligne. Pour cela, vous devez souscrire un contrat d'hébergement auprès d'un fournisseur d'hébergement web.</p><h2>Les solutions alternatives :</h2><h3>Les plateformes de création de site internet :</h3><p>Il existe de nombreuses plateformes de création de site internet qui permettent de créer un site internet sans avoir besoin de connaissances en programmation. Ces plateformes proposent des modèles de sites internet prédéfinis que vous pouvez personnaliser à votre guise. Les plus connues sont Wix ou Webflow.</p><h3>Les CMS :</h3><p>Les CMS (Content Management System ou Système de Gestion de Contenu) sont des outils qui permettent de créer et de gérer un site internet sans avoir besoin de connaissances en programmation. Les CMS les plus populaires sont WordPress, Drupal et Joomla.</p><br></br><p>Si vous souhaitez créer un site internet à Pont-à-Mousson, vous pouvez faire appel à une agence web locale. Celle-ci pourra vous accompagner dans toutes les étapes de la création de votre site internet, de la conception à la mise en ligne. Vous pouvez également créer votre site internet vous-même en utilisant une plateforme de création de site internet ou un CMS.</p>",
+};
+
+const article2 = {
+  title: "Pourquoi créer un site internet aujourd’hui ?",
+  catchPhrase:
+    "<p>Entreprises, magasins ou artisans qui souhaite se faire connaître et développer son activité.</p>",
+  text: "<p className='textHighlighted'>Avoir un site internet vitrine est aujourd'hui indispensable pour toute entreprise, magasin ou un artisan qui souhaite se faire connaître et développer son activité.</p><h2>Développer sa visibilité</h2><p>Un site internet est un excellent moyen de se faire connaître auprès de nouveaux clients. Il permet d'être présent en ligne 24h/24 et 7j/7, et d'être visible par un public potentiellement très large.</p><h3>Générer du trafic</h3><p>Un site internet permet d'attirer des visiteurs qui peuvent potentiellement devenir des clients. Pour cela, il est important de bien référencer son site internet sur les moteurs de recherche.</p><h3>Promouvoir ses produits ou services</h3><p>Un site internet est un excellent moyen de présenter ses produits ou services de manière détaillée. Il permet également de mettre en avant ses offres promotionnelles et de faciliter les achats en ligne.</p><h3>Communiquer avec ses clients</h3><p>Un site internet permet de communiquer avec ses clients de manière simple et efficace. Il permet de répondre à leurs questions, de recueillir leurs avis et de leur proposer des services complémentaires.</p><h3>Développer son activité</h3><p>Un site internet peut contribuer au développement de l'activité d'une entreprise, d'un magasin ou d'un artisan. Il permet d'augmenter les ventes, de gagner de nouveaux clients et de se différencier de la concurrence.</p><h2>Les différentes fonctionnalités d'un site internet</h2><p>Un site internet peut comporter différentes fonctionnalités, en fonction des besoins de l'entreprise, du magasin ou de l'artisan. Les fonctionnalités les plus courantes sont les suivantes :</p><h3>Une page d'accueil</h3><p>La page d'accueil est la première page que les visiteurs verront. Elle doit être claire et concise, et elle doit donner envie de poursuivre la visite du site.</p><h3>Une page de présentation</h3><p>La page de présentation doit présenter l'entreprise, le magasin ou l'artisan. Elle doit inclure des informations sur les produits ou services proposés, les valeurs de l'entreprise et les équipes.</p><h3>Une page de contact</h3><p>La page de contact doit permettre aux visiteurs de contacter l'entreprise, le magasin ou l'artisan. Elle doit inclure des informations sur les coordonnées, les horaires d'ouverture et les moyens de contact.</p><h3>Une boutique en ligne</h3><p>Si l'entreprise, le magasin ou l'artisan vend des produits en ligne, il est nécessaire de créer une boutique en ligne. La boutique en ligne doit être facile à utiliser et à naviguer.</p><h3>Un blog</h3><p>Un blog est un excellent moyen de partager des informations et des actualités avec ses clients. Il permet également de générer du trafic sur le site internet.</p><br></br><p>Un site internet est un outil indispensable pour toute entreprise, un magasin ou un artisan qui souhaite se faire connaître et développer son activité. Il permet de développer sa visibilité, de générer du trafic, de promouvoir ses produits ou services, de communiquer avec ses clients et de développer son activité à Pont à mousson.</p>",
 };
 
 const articlesData = [
@@ -21,6 +28,20 @@ const articlesData = [
     updatedAt: "2023-10-27T12:00:00+02:00",
     tag: "Création site internet à Pont-à-mousson",
     readingTime: 1,
+  },
+  {
+    id: 2,
+    slug: "pourquoi-créer-un-site-internet",
+    ...article2,
+    picture: {
+      src: code,
+      alt: "Affichage code de programmation sur un écran",
+      title: "Affichage code de programmation sur un écran",
+    },
+    publishedAt: "2023-11-02T12:00:00+02:00",
+    updatedAt: "2023-11-02T12:00:00+02:00",
+    tag: "Pourquoi créer un site internet à Pont à mousson",
+    readingTime: 2,
   },
 ];
 

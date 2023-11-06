3457a0158c0784a296a7a5454c1738aa48e3b34b
thiebautremy <remythiebaut@msn.com>
2023-11-06 13:14:44 +0100

    update présentation

--- components/Presentation/Presentation.tsx
+++ components/Presentation/Presentation.tsx
@@ -20,20 +20,45 @@ const Presentation = () => {
             <strong>Pont-à-Mousson</strong> ?
           </h2>
           <div className={styles.content}>
+            <h3>Mes compétences</h3>
             <p>
-              <br></br>J&apos;offre des solutions personnalisées pour la
-              conception de sites vitrine, adaptés à vos besoins et à votre
-              budget. <br></br>Ma passion est de donner vie à votre présence en
+              <br></br>Fort de plusieurs années en tant que développeur web
+              j&apos;offre des solutions personnalisées pour la conception de
+              sites vitrine, adaptés à vos besoins et à votre budget et ainsi
+              promouvoir sur le web votre identité visuelle, vos idées et votre
+              marque. <br></br>Ma passion est de donner vie à votre présence en
               ligne en créant des sites web esthétiques, intuitifs et
               performants. Que vous soyez une entreprise locale ou un
               particulier, je mettrai en avant votre identité unique.
             </p>
             <p>
+              Créer un site internet demande aujourd&apos;hui beaucoup de
+              compétences que j&apos;ai pu acquérir en développement web. La
+              création, le design, la conception de logo, le responsive design
+              (visuel adapté à toutes les tailles d&apos;écrans), le choix du
+              nom de domaine, le référencement naturel (capacité du site à être
+              bien placé dans les résultats sur les moteurs de recherche) ainsi
+              que l&apos;hébergement sont autant de critères à prendre en compte
+              et maitrisé afin de garantir le meilleur produit en tenant en
+              compte le budget dédié.
+            </p>
+            <h3>Mon savoir faire</h3>
+            <p>
               {" "}
               Avoir un site internet c&apos;est bien mais qu&apos;il soit
               visible c&apos;est encore mieux. Je m&apos;engage pour assurer la
               visibilité de votre site via le référencement naturel (SEO), vous
-              permettant d&apos;atteindre votre public cible plus facilement.
+              permettant d&apos;atteindre votre public cible plus facilement.{" "}
+            </p>
+            <p>
+              <strong>
+                Vous souhaitez pouvoir gérer et modifier vous même votre contenu
+                publié ?
+              </strong>{" "}
+              <br></br>
+              <br></br>Alors nous pouvons mettre en place une solution via un
+              CMS (Content Management System) afin de vous donner la main sur
+              chaque mots présents sur votre site internet.
             </p>
             <p>
               {" "}
@@ -42,7 +67,7 @@ const Presentation = () => {
               digitale pour renforcer votre présence en ligne. Choisissez une
               agence qui comprend vos besoins et qui excelle dans la création de
               sites web à Pont-à-Mousson. Contactez-moi dès aujourd&apos;hui
-              pour démarrer votre projet en ligne !
+              pour démarrer votre projet web !
             </p>
           </div>
           <CTAContainer />

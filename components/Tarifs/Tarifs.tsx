3b621c4d359c755dacbe566d6ebee94f7f7dd188
thiebautremy <remythiebaut@msn.com>
2023-11-14 15:18:05 +0100

    update tarif

--- components/Tarifs/Tarifs.tsx
+++ components/Tarifs/Tarifs.tsx
@@ -4,6 +4,8 @@ import { motion } from "framer-motion";
 import cx from "classnames";
 import { useState } from "react";
 import Link from "next/link";
+import { GiTRexSkull } from "react-icons/gi";
+import CTAContainer from "../CTAContainer/CTAContainer";
 
 type TarifCardType = {
   id: number;
@@ -17,36 +19,38 @@ const Tarifs = () => {
   const tarifsCardsData: TarifCardType[] = [
     {
       id: 1,
-      title: "Site vitrine",
+      title: "Formule Starter",
       price: "600 €",
       hover: false,
       className: "",
       description: [
         "Hébergement et nom de domaine en .fr",
+        "Un design personnalisé",
         "Page d’accueil, de contact, de présentation",
-        "maintenance de 3 mois",
+        "Maintenance de 6 mois",
       ],
     },
     {
       id: 2,
-      title: "Site vitrine +",
+      title: "Formule Pro",
       price: "900 €",
       hover: false,
       className: "",
       description: [
         "Hébergement et nom de domaine en .fr",
-        "Gestion de contenu sous Wordpress",
+        "Un design personnalisé",
         "Page d’accueil, de contact, de présentation",
-        "maintenance de 6 mois",
+        "Stratégie de référencement avancé",
+        "Maintenance de 12 mois",
       ],
     },
     {
       id: 3,
-      title: "Personnalisé",
+      title: "Formule Premium",
       price: "A définir",
       hover: false,
       className: "",
-      description: ["A définir selon besoin"],
+      description: ["Développement sur mesure"],
     },
   ];
 
@@ -91,7 +95,8 @@ const Tarifs = () => {
         <Container margin="isHuge">
           <h1>Tarifs des prestations de Webosaurus</h1>
           <h2>
-            Voici les tarifs de nos prestations web à{" "}
+            Webosaurus, agence web, propose différentes formules de tarifs pour
+            répondre aux besoin de tous les budgets {""}
             <strong>Pont-à-Mousson</strong> ?
           </h2>
           <div className={styles.content}>
@@ -112,13 +117,60 @@ const Tarifs = () => {
                         <li key={item}>{item}</li>
                       ))}
                     </ul>
-                    <Link href={"/contact"} className="ctaBtn ctaBtnScrolled">
+                    <Link
+                      href={"/contact"}
+                      className={cx(
+                        `ctaBtn ctaBtnScrolled ${styles.contactBtn}`
+                      )}
+                    >
                       <span>Me contacter</span>
                     </Link>
                   </div>
                 ))}
             </div>
+            <ul className={styles.formulasList}>
+              <li className={styles.formula}>
+                <GiTRexSkull className={styles.TRexSkull} />
+                <strong>La formule Starter</strong> est idéale pour les petites
+                entreprises ou les particuliers qui souhaitent créer un site
+                internet simple et efficace.
+              </li>
+              <li className={styles.formula}>
+                <GiTRexSkull className={styles.TRexSkull} />
+                <strong>La formule Pro</strong> est recommandée pour les
+                entreprises ou les professionnels qui souhaitent créer un site
+                internet plus complet, plus performant et mieux référencé sur
+                les moteurs de recherche.
+              </li>
+              <li className={styles.formula}>
+                <GiTRexSkull className={styles.TRexSkull} />
+                <strong>La formule Premium</strong> est la solution idéale pour
+                les entreprises ou les professionnels qui souhaitent créer un
+                site internet haut de gamme et sur mesure.
+              </li>
+            </ul>
+            <h2>Pourquoi choisir Webosaurus ?</h2>
+            <p>
+              Webosaurus est une agence web qui propose des services de qualité
+              à des tarifs abordables. L&apos;agence accompagne ses clients à
+              chaque étape du processus de création de site internet, de la
+              conception à la maintenance.
+            </p>
+            <p>
+              Les avantages de choisir Webosaurus pour la création de votre site
+              internet comprennent :
+            </p>
+            <ul className={styles.benefitsList}>
+              <li className={styles.benefit}>Un design personnalisé</li>
+              <li className={styles.benefit}>Un développement sur mesure</li>
+              <li className={styles.benefit}>
+                Un référencement naturel (SEO) efficace
+              </li>
+              <li className={styles.benefit}>Une maintenance de qualité</li>
+              <li className={styles.benefit}>Un accompagnement personnalisé</li>
+            </ul>
           </div>
+          <CTAContainer />
         </Container>
       </motion.div>
     </div>

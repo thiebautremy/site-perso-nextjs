1df5cb9d493a56ad63a3368776803c2175d9a1ab
thiebautremy <remythiebaut@msn.com>
2023-11-17 10:41:45 +0100

    add gtm

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,6 +29,14 @@ export default function Document() {
           id="myScript"
           strategy="lazyOnload"
         />
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+        >{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TDVB5LLZ')`}</Script>
         <meta property="og:type" content="website" />
 
         <meta name="robots" content="index" />
@@ -38,6 +46,11 @@ export default function Document() {
         />
       </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TDVB5LLZ" height="0" width="0" style="display: none; visibility: hidden;" />`,
+          }}
+        />
         <Main />
         <NextScript />
       </body>

1c657273b4c66164559ac68829d0e7a681d6251d
thiebautremy <remythiebaut@msn.com>
2023-12-03 16:00:35 +0100

    update animation

--- components/Layout/AnimationLayout.tsx
+++ components/Layout/AnimationLayout.tsx
@@ -1,16 +1,5 @@
 import { useState, useEffect, ReactNode } from "react";
-import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
-
-const boxVariant = {
-  visible: {
-    opacity: 1,
-    scale: 1,
-    transition: { duration: 0.8 },
-    ease: "easeInOut",
-  },
-  hidden: { opacity: 0, scale: 0.9 },
-};
+import { motion } from "framer-motion";
 
 type AnimationLayoutProps = {
   children: ReactNode;
@@ -24,30 +13,15 @@ const AnimationLayout: React.FC<AnimationLayoutProps> = ({ children }) => {
     setIsMobile(width < 768);
   }, []);
 
-  const control = useAnimation();
-  const [ref, inView] = useInView();
-
-  useEffect(() => {
-    if (inView) {
-      control.start("visible");
-    } else {
-      control.start("hidden");
-    }
-  }, [control, inView]);
-
   return isMobile ? (
     <div>{children}</div>
   ) : (
     <motion.div
-      // initial="hidden"
-      // variants={boxVariant}
-      // ref={ref}
-      // animate={control}
       initial={{ opacity: 0, scale: 0.8 }}
       whileInView={{ opacity: 1, scale: 1 }}
       whileTap={{ opacity: 0, scale: 0.8 }}
-      viewport={{ once: false, amount: 0.3 }}
-      transition={{ duration: 0.8, ease: "easeInOut" }}
+      viewport={{ once: true, amount: 0.3 }}
+      transition={{ duration: 0.6, ease: "easeInOut" }}
     >
       {children}
     </motion.div>
